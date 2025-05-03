e84bb30a3597de6d1d258303b4858790d06a2a4f
Suresh M <sc222810@gmail.com>
2025-05-03 18:04:19 +0530

    Completed the auth setup

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,3 +3,12 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return <SignIn />;
 }
+
+// import AuthForm from "@/components/AuthForm";
+// import React from "react";
+
+// const page = () => {
+//   return <AuthForm type="sign-in" />;
+// };
+
+// export default page;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,3 +3,12 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return <SignUp />;
 }
+
+// import AuthForm from "@/components/AuthForm";
+// import React from "react";
+
+// const page = () => {
+//   return <AuthForm type="sign-up" />;
+// };
+
+// export default page;
